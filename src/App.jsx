8892e4a0869753bca6c073c83ca906e47457370d
Oaes Kuruni <oaeskuruni27@gmail.com>
2026-04-18 16:01:50 +0600

    Rating part added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Banner from "./components/Banner";
 import { ToastContainer } from "react-toastify";
 import { useState } from "react";
 import NavBar from "./components/NavBar";
+import Rating from "./components/Rating";
 
 function App() {
   const [cartItems] = useState([]);
@@ -13,6 +14,7 @@ function App() {
 
       <NavBar cartCount={cartItems.length}></NavBar>
       <Banner />
+      <Rating></Rating>
     </>
   );
 }
